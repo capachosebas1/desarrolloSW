eb5ad6d0bd6eb3bf49ca339f1e420e50329f8282
capachosebas1 <84109547+capachosebas1@users.noreply.github.com>
2022-04-22 20:26:02 -0500

    cam
    cambios

---
+++ Serie.ts
@@ -0,0 +1,5 @@
+export class Serie{
+    constructor(public id: number , public nombre:string, public productora: string, public seasons: number, public description: string,public link: string, public imagen: string){
+
+    }
+}
